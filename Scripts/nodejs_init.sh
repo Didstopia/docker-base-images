b120a774469547772363118c3ebf48e08403e3fb
Pauli Jokela <pauli.jokela@didstopia.com>
2017-05-13 23:26:22 +0300

    Fleshing out more basic functionality, as well as fixing issues as we go on.

---
+++ Scripts/nodejs_init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+echo "Initializing.."
+
+cd "/app" ||
+{
+    echo "Application directory is missing: /app"
+    exit 1
+}
+
+if [ -f /app/package.json ]
+then
+    echo "Installing dependencies.."
+    npm install
+    if jq -e ".scripts.build" package.json >/dev/null; then
+        echo "Building.."
+        npm run build
+    fi
+    if jq -e ".scripts.start" package.json >/dev/null; then
+        echo "Starting.."
+        npm start
+    else
+        echo "Application configuration is missing 'start' script: /app/package.json"
+        exit 1
+    fi
+else
+    echo "Application configuration is missing: /app/package.json"
+    exit 1
+fi
--- docker-make.sh
+++ docker-make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm -w /usr/src/app -v ~/.docker:/root/.docker -v /var/run/docker.sock:/var/run/docker.sock -v "$(pwd)":/usr/src/app jizhilong/docker-make docker-make "$@"
+docker run --rm=true -w /usr/src/app -v ~/.docker:/root/.docker -v /var/run/docker.sock:/var/run/docker.sock -v "$(pwd)":/usr/src/app jizhilong/docker-make docker-make "$@"
 
 ## TODO: Remove once Docker Make is not generating leftover containers
 docker rm $(docker ps -a -q) >/dev/null 2>&1
