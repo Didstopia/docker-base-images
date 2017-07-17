6610706ac2e269d2e63d3b5ffb581ac54042e041
Pauli Jokela <pauli.jokela@didstopia.com>
2017-07-17 09:45:27 +0300

    docker-make now supports removing intermediate build containers

--- docker-make.sh
+++ docker-make.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 docker run --rm=true -w /usr/src/app -v ~/.docker:/root/.docker -v /var/run/docker.sock:/var/run/docker.sock -v "$(pwd)":/usr/src/app jizhilong/docker-make docker-make "$@"
-
-## TODO: Remove once Docker Make is not generating leftover containers
-docker rm $(docker ps -a -q) >/dev/null 2>&1
