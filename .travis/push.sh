67aaa8c56dcd6df813591069c19357f476762f0e
Pauli Jokela <pauli.jokela@didstopia.com>
2017-07-18 14:08:03 +0300

    Fixing Travis PR detection

--- .travis/push.sh
+++ .travis/push.sh
@@ -17,49 +17,48 @@ if [[ ! -z ${TRAVIS_PULL_REQUEST+x} && "${TRAVIS_PULL_REQUEST}" != "false" ]]; t
     echo ""
     echo "NOTICE: Pull request detected, skipping push.."
     echo ""
-    exit 0
-fi
-
-# Build the images
-echo ""
-echo "Pushing images.."
-
-## TODO: Figure out how to properly redirect docker-make's output to /dev/null
-
-echo ""
-echo "  * Ubuntu 14.04"
-if [ "$UPDATE_UBUNTU_14_04" == "1" ]; then
-    echo ""
-    eval $(./docker-make.sh -f .docker-make.ubuntu-14-04.yml) >/dev/null 2>&1
 else
-    echo -n "    > No update necessary, skipping.."
-fi
-echo ""
-
-echo ""
-echo "  * Ubuntu 16.04"
-if [ "$UPDATE_UBUNTU_16_04" == "1" ]; then
+    # Build the images
     echo ""
-    eval $(./docker-make.sh -f .docker-make.ubuntu-16-04.yml) >/dev/null 2>&1
-else
-    echo -n "    > No update necessary, skipping.."
-fi
-echo ""
+    echo "Pushing images.."
 
-echo ""
-echo "  * Alpine 3.5"
-if [ "$UPDATE_ALPINE_3_5" == "1" ]; then
+    ## TODO: Figure out how to properly redirect docker-make's output to /dev/null
+
     echo ""
-    eval $(./docker-make.sh -f .docker-make.alpine-3-5.yml) >/dev/null 2>&1
-else
-    echo -n "    > No update necessary, skipping.."
+    echo "  * Ubuntu 14.04"
+    if [ "$UPDATE_UBUNTU_14_04" == "1" ]; then
+        echo ""
+        eval $(./docker-make.sh -f .docker-make.ubuntu-14-04.yml) >/dev/null 2>&1
+    else
+        echo -n "    > No update necessary, skipping.."
+    fi
+    echo ""
+
+    echo ""
+    echo "  * Ubuntu 16.04"
+    if [ "$UPDATE_UBUNTU_16_04" == "1" ]; then
+        echo ""
+        eval $(./docker-make.sh -f .docker-make.ubuntu-16-04.yml) >/dev/null 2>&1
+    else
+        echo -n "    > No update necessary, skipping.."
+    fi
+    echo ""
+
+    echo ""
+    echo "  * Alpine 3.5"
+    if [ "$UPDATE_ALPINE_3_5" == "1" ]; then
+        echo ""
+        eval $(./docker-make.sh -f .docker-make.alpine-3-5.yml) >/dev/null 2>&1
+    else
+        echo -n "    > No update necessary, skipping.."
+    fi
+    echo ""
+
+    # Disable error handling (useful when running with "source")
+    set +e
+    set +o pipefail
+
+    echo ""
+    echo "Push completed successfully."
+    echo ""
 fi
-echo ""
-
-# Disable error handling (useful when running with "source")
-set +e
-set +o pipefail
-
-echo ""
-echo "Push completed successfully."
-echo ""
--- .travis/update_check.sh
+++ .travis/update_check.sh
@@ -17,70 +17,69 @@ if [[ ! -z ${TRAVIS_PULL_REQUEST+x} && "${TRAVIS_PULL_REQUEST}" != "false" ]]; t
     echo ""
     echo "NOTICE: Pull request detected, skipping update check.."
     echo ""
-    exit 0
-fi
-
-# Environment variables exported at the end of the script
-UPDATE_UBUNTU_16_04=0
-UPDATE_UBUNTU_14_04=0
-UPDATE_ALPINE_3_5=0
-
-# Check each image for updates and set an environment
-# variable that's then used in the build script
-echo ""
-echo "Checking images for updates.."
-
-echo ""
-echo "  * Ubuntu 14.04"
-echo -n "    > Checking for updates.. "
-if docker run --name test -it --rm didstopia/base:ubuntu-14.04 bash -c "apt-get update > /dev/null && apt-get --just-print upgrade | grep \"Inst \"" | grep "Inst " > /dev/null
-then
-    echo -n "updates available"
-    echo ""
-    UPDATE_UBUNTU_14_04=1
 else
-	echo -n "no updates available"
-	echo ""
-fi
+    # Environment variables exported at the end of the script
+    UPDATE_UBUNTU_16_04=0
+    UPDATE_UBUNTU_14_04=0
+    UPDATE_ALPINE_3_5=0
 
-echo ""
-echo "  * Ubuntu 16.04"
-echo -n "    > Checking for updates.. "
-if docker run --name test -it --rm didstopia/base:ubuntu-16.04 bash -c "apt-get update > /dev/null && apt-get --just-print upgrade | grep \"Inst \"" | grep "Inst " > /dev/null
-then
-    echo -n "updates available"
+    # Check each image for updates and set an environment
+    # variable that's then used in the build script
     echo ""
-    UPDATE_UBUNTU_16_04=1
-else
-	echo -n "no updates available"
-	echo ""
-fi
+    echo "Checking images for updates.."
 
-echo ""
-echo "  * Alpine 3.5"
-echo -n "    > Checking for updates.. "
-if docker run --name test -it --rm didstopia/base:alpine-3.5 /bin/ash -c "apk update > /dev/null && apk upgrade | grep \"Upgrading \"" | grep "Upgrading " > /dev/null
-then
-    echo -n "updates available"
     echo ""
-    UPDATE_ALPINE_3_5=1
-else
-	echo -n "no updates available"
-	echo ""
+    echo "  * Ubuntu 14.04"
+    echo -n "    > Checking for updates.. "
+    if docker run --name test -it --rm didstopia/base:ubuntu-14.04 bash -c "apt-get update > /dev/null && apt-get --just-print upgrade | grep \"Inst \"" | grep "Inst " > /dev/null
+    then
+        echo -n "updates available"
+        echo ""
+        UPDATE_UBUNTU_14_04=1
+    else
+        echo -n "no updates available"
+        echo ""
+    fi
+
+    echo ""
+    echo "  * Ubuntu 16.04"
+    echo -n "    > Checking for updates.. "
+    if docker run --name test -it --rm didstopia/base:ubuntu-16.04 bash -c "apt-get update > /dev/null && apt-get --just-print upgrade | grep \"Inst \"" | grep "Inst " > /dev/null
+    then
+        echo -n "updates available"
+        echo ""
+        UPDATE_UBUNTU_16_04=1
+    else
+        echo -n "no updates available"
+        echo ""
+    fi
+
+    echo ""
+    echo "  * Alpine 3.5"
+    echo -n "    > Checking for updates.. "
+    if docker run --name test -it --rm didstopia/base:alpine-3.5 /bin/ash -c "apk update > /dev/null && apk upgrade | grep \"Upgrading \"" | grep "Upgrading " > /dev/null
+    then
+        echo -n "updates available"
+        echo ""
+        UPDATE_ALPINE_3_5=1
+    else
+        echo -n "no updates available"
+        echo ""
+    fi
+
+    echo ""
+    echo -n "Exporting results as environment variables.. "
+    export UPDATE_UBUNTU_14_04
+    export UPDATE_UBUNTU_16_04
+    export UPDATE_ALPINE_3_5
+    echo -n "done"
+    echo ""
+
+    # Disable error handling (useful when running with "source")
+    set +e
+    set +o pipefail
+
+    echo ""
+    echo "Update check completed successfully."
+    echo ""
 fi
-
-echo ""
-echo -n "Exporting results as environment variables.. "
-export UPDATE_UBUNTU_14_04
-export UPDATE_UBUNTU_16_04
-export UPDATE_ALPINE_3_5
-echo -n "done"
-echo ""
-
-# Disable error handling (useful when running with "source")
-set +e
-set +o pipefail
-
-echo ""
-echo "Update check completed successfully."
-echo ""
